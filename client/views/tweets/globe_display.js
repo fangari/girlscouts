a06d83176da5134ebe5a13a252074f2d6aa5c2b0
Fernando Angarita <fangari@gmail.com>
2014-09-17 17:06:55 -0500

    Adds route for the globe display and necessary data connections.

---
+++ client/views/tweets/globe_display.js
@@ -0,0 +1,5 @@
+Template.globeDisplay.helpers ({
+  message: function() {
+    return Messages.findOne();
+  }
+});
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,12 @@
 Meteor.publish('messages', function(options) {
   return Messages.find({}, options);
 });
+
+Meteor.publish('globe-messages', function() {
+  return Messages.find({reviewed: true, origin: 'globe'},
+                       {sort: {created_at: -1, _id: -1, viewCount: -1}});
+});
+
+Meteor.publish('stats', function() {
+  return Stats.find({}, {sort: {viewCount: -1, _id: -1}});
+});

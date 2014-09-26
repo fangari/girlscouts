6e94c6f40912874607e3b75b913463a737e781f5
Fernando Angarita <fangari@gmail.com>
2014-09-26 18:37:57 -0500

    Adds flip animation and queuing of individual stories.

--- client/views/tweets/story.js
+++ client/views/tweets/story.js
@@ -1,12 +1,61 @@
+var storyAnimation = {
+  storyId: '',
+  dep: new Tracker.Dependency(),
+  get: function() {
+    this.dep.depend();
+    return Stories.findOne({_id: this.storyId});
+  },
+  set: function(selectedStoryId) {
+    this.storyId = selectedStoryId;
+    this.dep.changed();
+  }
+};
+
+Template.story.helpers({
+  story: function() {
+    return storyAnimation.get();
+  }
+});
+
 Template.story.rendered = function() {
+
+  var storiesQueue = new buckets.Queue();
+
+  var flipit = function(card) {
+    card.children('.faces').toggleClass('flipped');
+  };
+
+  var animateStory = function() {
+    var $card = $(".story-card");
+    $card.fadeIn(2000);
+    Meteor.setTimeout(function() {
+      flipit($card);
+    }, 3000);
+    Meteor.setTimeout(function() {
+      flipit($card);
+    }, 9000);
+    $card.delay(6000).fadeOut(2000);
+  };
+
+  var animateCallToAction = function() {
+    var $callAnimation = $('.call-animation-js');
+    $callAnimation.text('Come and see what the Girl Scouts have offered thousands like you.');
+    $callAnimation.slideDown(2000);
+    $callAnimation.delay(5500).slideUp(2000);
+  };
+
   Stories.find().observeChanges({
     changed: function(id, fields) {
-      animateStory(Stories.findOne({_id: id}));
+      storiesQueue.add(id);
     }
   });
 
-  var animateStory = function(story) {
-    console.log("The selected story is", story);
-  };
-
+  Meteor.setInterval(function() {
+    if(!storiesQueue.isEmpty()) {
+      storyAnimation.set(storiesQueue.dequeue());
+      animateStory();
+    } else {
+      animateCallToAction();
+    }
+  }, 11500);
 };

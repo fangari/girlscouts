d5c8d19a8c600e543014a67d3f6315ff480a658d
Fernando Angarita <fangari@gmail.com>
2014-10-09 15:00:20 -0500

    Initializes the collections on startup, not from fixtures.

--- server/server.js
+++ server/server.js
@@ -7,4 +7,29 @@ Meteor.startup(function() {
     access_token_key: conf.access_token.key,
     access_token_secret: conf.access_token.secret
   });
+  if (Meteor.users.find().count() === 0) {
+    var accountsFile = {};
+    accountsFile = JSON.parse(Assets.getText('accounts.json'));
+    _.forEach(accountsFile.users, function(user){ Accounts.createUser(user); });
+  }
+
+  if (Stats.find().count() === 0) {
+    var statsFile = {};
+    statsFile = JSON.parse(Assets.getText('stats.json'));
+    _.forEach(statsFile.stats, function(stat) { Stats.insert(stat); });
+  }
+
+  if(Stories.find().count() === 0) {
+    var storiesFile = {};
+    storiesFile = JSON.parse(Assets.getText('stories.json'));
+    _.forEach(storiesFile.stories, function(story) { Stories.insert(story); });
+  }
+
+  if (WordCloud.find().count() === 0) {
+    var wordsForCloud = {};
+    wordsForCloud = JSON.parse(Assets.getText('words_for_cloud.json'));
+    _.forEach(wordsForCloud.words, function(wordDocument) {
+      WordCloud.insert(wordDocument);
+    });
+  }
 });
